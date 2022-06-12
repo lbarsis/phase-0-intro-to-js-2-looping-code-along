527ff68499e5dcf6f55840380d07c601dfb3212d
Levi Barsis <lbarsis@gmail.com>
2022-06-11 21:16:56 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, gift) {
+    let arr = [];
+    for (let i=0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${gift} gift!`;
+        arr[i] = message
+    }
+    return arr
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
